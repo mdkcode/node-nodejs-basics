5a094313d2441b8529cf2a16485023e54c234d7c
Madina <madinakrgl@gmail.com>
2025-04-24 00:42:11 +0500

    feat(): first fs subtask

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import fs from "fs";
+
+const fileName = new URL("./files/fresh.txt", import.meta.url);
+const fileContent = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+  fs.access(fileName, fs.constants.F_OK | fs.constants.W_OK, (err) => {
+    if (err && err.code === "ENOENT") {
+      fs.writeFile(fileName, fileContent, (err) => err && console.error(err));
+    } else throw new Error("FS operation failed");
+  });
 };
 
 await create();
