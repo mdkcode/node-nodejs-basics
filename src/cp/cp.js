ce246f8286c1dbf1d52ed43cf38f51b0aa8c8751
Madina <madinakrgl@gmail.com>
2025-04-25 16:45:59 +0500

    feat(): add cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
+import { spawn } from "child_process";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const scriptPath = path.join(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = spawn("node", [scriptPath, ...args], {
+    stdio: ["pipe", "pipe", "pipe", "ipc"],
+  });
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["sdkjbd", "wefkjlh"]);
